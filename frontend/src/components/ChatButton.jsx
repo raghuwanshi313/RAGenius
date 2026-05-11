b8b926a09ee5566557bd154b76b517886286fd18
AnkitVS0702 <ankitshiksharthi@gmail.com>
2026-05-11 17:25:52 +0530

    fixed login-forms ui , added chatbot logo and requirements.txt in backend folder

--- frontend/src/components/ChatButton.jsx
+++ frontend/src/components/ChatButton.jsx
@@ -4,12 +4,13 @@ import Magnet from "./ui/Magnet";
 
 function ChatButton() {
   return (
-    <div className="fixed bottom-8 right-8">
+    <div className="fixed bottom-8 right-8 flex flex-col items-center">
       <Magnet>
         <button>
           <RiChatAiFill className="text-4xl text-primary lg:text-6xl" />
         </button>
       </Magnet>
+      <span className="mt-2 text-xs text-gray-500">Press Ctrl + /</span>
     </div>
   );
 }
